bf54f251c63a7b5b7c9f9b7129900ced09de3c2c
Matt Chesler <mchesler@movableink.com>
2020-06-03 16:06:12 -0400

    Add global modified to domain cleansing regex

--- middlewares/set_domain.js
+++ middlewares/set_domain.js
@@ -6,7 +6,7 @@ const setDomain = function(domains) {
     if (req.vdomain) {
       next();
     } else {
-      let cleanedDomain = host.replace(/[^A-Za-z0-9\-.:]/, "");
+      let cleanedDomain = host.replace(/[^A-Za-z0-9\-.:]/g, "");
       res.writeHead(500, {});
       res.end("Unrecognized domain " + cleanedDomain + " and no default domain set");
     }
